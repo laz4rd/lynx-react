bb19cedcf63654c1748f28441baddca92804884f
Brijraj Singh Bhati <114866638+laz4rd@users.noreply.github.com>
2025-06-23 20:59:11 +0530

    modified:   app/page.tsx 	new file:   components/ui/breadcrumb.tsx 	new file:   components/ui/separator.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -23,97 +23,115 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
 
 export default function HomePage() {
-  const [links, setLinks] = useState<{ title: string; url: string }[]>([])
-  const [form, setForm] = useState({ title: "", url: "" })
-
-  const handleAddLink = () => {
-    if (form.title && form.url) {
-      setLinks([...links, form])
-      setForm({ title: "", url: "" })
-    }
-  }
 
   return (
+    
     <div className="max-w-md w-full space-y-4">
+      <Breadcrumb>
+  <BreadcrumbList>
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/">X4 Card</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/components">...</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/components">Brijraj Singh Bhati</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbPage>Lynx</BreadcrumbPage>
+    </BreadcrumbItem>
+  </BreadcrumbList>
+</Breadcrumb>
+
       <Avatar className="w-20 h-20 mx-auto">
         <AvatarImage src="app/assets/x4-256.png" />
         <AvatarFallback>BS</AvatarFallback>
       </Avatar>
       <h1 className="text-xl font-bold text-center">Brijraj Singh Bhati</h1>
-      <p className="text-gray-500 text-center">Creating a minimal link hub</p>
-
-      
+      <p className="text-gray-500 text-center">Creating a minimal link hub</p>      
 
       <div className="space-y-2">
             <Card>
-  <CardHeader>
-    <CardTitle>X4 Creative</CardTitle>
-    <CardDescription>Personal Portfolio</CardDescription>
-    <CardAction>Webpage</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>X4 is a personal space where I curate and showcase my work, serving as a comprehensive portfolio that highlights my creativity, technical skills, and innovative projects.</p>
-    </CardContent>
-    <CardFooter>
-      <Drawer>
-        <DrawerTrigger className="w-full" asChild>
-          <Button>Visit</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Proceed to link</DrawerTitle>
-            <DrawerDescription>You Will Now Visit X4 Creatives Page</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button asChild>
-              <a href="https://x4creative.framer.website">Proceed</a>
-            </Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </CardFooter>
-  </Card>
+              <CardHeader>
+                <CardTitle>X4 Creative</CardTitle>
+                <CardDescription>Personal Portfolio</CardDescription>
+                <CardAction>Webpage</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>X4 is a personal space where I curate and showcase my work, serving as a comprehensive portfolio that highlights my creativity, technical skills, and innovative projects.</p>
+                </CardContent>
+                <CardFooter>
+                  <Drawer>
+                    <DrawerTrigger className="w-full" asChild>
+                      <Button>Visit</Button>
+                    </DrawerTrigger>
+                    <DrawerContent>
+                      <DrawerHeader>
+                        <DrawerTitle>Proceed to link</DrawerTitle>
+                        <DrawerDescription>You Will Now Visit X4 Creatives Page</DrawerDescription>
+                      </DrawerHeader>
+                      <DrawerFooter>
+                        <Button asChild>
+                          <a href="https://x4creative.framer.website">Proceed</a>
+                        </Button>
+                        <DrawerClose>
+                          <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                      </DrawerFooter>
+                    </DrawerContent>
+                  </Drawer>
+                </CardFooter>
+              </Card>
 
 
-  <Card>
-  <CardHeader>
-    <CardTitle>X4MD</CardTitle>
-    <CardDescription>AI-Assisted Command Line Interface</CardDescription>
-    <CardAction>Software</CardAction>
-  </CardHeader>
-  <CardContent>
-    <p>X4MD is a AI-Assisted Command Line Interface written in python using Ollama's lightweight Gemma3:1b Model</p>
-    </CardContent>
-    <CardFooter>
-      <Drawer>
-        <DrawerTrigger className="w-full" asChild>
-          <Button>Visit</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Proceed to link</DrawerTitle>
-            <DrawerDescription>You Will Now Visit X4MD</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button asChild>
-              <a href="https://x4md.framer.website">Proceed</a>
-            </Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </CardFooter>
-  </Card>
+              <Card>
+              <CardHeader>
+                <CardTitle>X4MD</CardTitle>
+                <CardDescription>AI-Assisted Command Line Interface</CardDescription>
+                <CardAction>Software</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>X4MD is a AI-Assisted Command Line Interface written in python using Ollama's lightweight Gemma3:1b Model</p>
+                </CardContent>
+                <CardFooter>
+                  <Drawer>
+                    <DrawerTrigger className="w-full" asChild>
+                      <Button>Visit</Button>
+                    </DrawerTrigger>
+                    <DrawerContent>
+                      <DrawerHeader>
+                        <DrawerTitle>Proceed to link</DrawerTitle>
+                        <DrawerDescription>You Will Now Visit X4MD</DrawerDescription>
+                      </DrawerHeader>
+                      <DrawerFooter>
+                        <Button asChild>
+                          <a href="https://x4md.framer.website">Proceed</a>
+                        </Button>
+                        <DrawerClose>
+                          <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                      </DrawerFooter>
+                    </DrawerContent>
+                  </Drawer>
+                </CardFooter>
+              </Card>
   
       </div>
     </div>
   )
 }
-  
